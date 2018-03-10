1716c3a012b78bfff800fd900086dd4c3c5edda5
Pardim93 <wellpardim10@gmail.com>
2018-03-10 05:07:02 -0300

    add mods

--- paddle.js
+++ paddle.js
@@ -4,6 +4,7 @@ class Paddle {
         this.w = 20;
         this.h = 100;
         this.ychange = 0;
+
         
         if (isLeft) {
             this.x = this.w;
@@ -23,8 +24,11 @@ class Paddle {
     
     show() {
         fill(255);
-        rectMode(CENTER);
-        rect(this.x, this.y, this.w, this.h);
+        // rectMode(CENTER);
+        // rect(3, 2, 55, 55);
+        rect(this.x, this.y, this.w, this.h,80, 80);
+        ellipse(this.x, this.y, this.r*2);
+
     }
 }
 
